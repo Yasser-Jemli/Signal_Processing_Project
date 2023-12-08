1e8a0c37f511333b46662c05182e541ad5f49fa1
yasser-jemli <yasserjamli37@gmail.com>
2023-12-08 16:35:54 +0100

    Implement FFT
    Signed-off-by: yasser-jemli <yasserjamli37@gmail.com>

--- _recording_Project/Voice_project.py
+++ _recording_Project/Voice_project.py
@@ -9,6 +9,10 @@ from python_speech_features import mfcc, delta
 from python_speech_features import logfbank
 import scipy.io.wavfile as wav
 
+#FFT 
+from IPython import get_ipython
+
+
 # ''' recording wave speech signal '''
 # => Code : 
 # fs = 16000  # Sample rate
@@ -42,3 +46,43 @@ plt.title('signal wav')
 plt.xlabel("Time [s]")
 plt.ylabel("Amplitude")
 plt.show()
+
+#  fft
+# ============================================================
+get_ipython().magic('reset -sf')
+
+plt.close('all')
+
+# ============================================================
+def nextpow2(N):
+    """ Function for finding the next power of 2 """
+    n = 1
+    while n < N: n *= 2
+    return n
+# ============================================================
+
+# # Calcul de la TFD avec la fft
+N  = len(x);
+Nf = nextpow2(N)
+Fe=fs
+
+X  =np.fft.fft(x,Nf)/N; 
+# on effectue un fftshift pour positionner la frequence zero au centre
+X  = np.fft.fftshift(X); 
+Xa = np.abs(X);
+
+#_ fp=-Fe/2:Fe/Nf:Fe/2-Fe/Nf; ==> # fp=np.arange(-Fe/2,Fe/2,Fe/Nf);
+#_ fp=[-Nf/2:Nf/2-1]*Fe/Nf; 
+fp=np.arange(-Nf/2,Nf/2)*Fe/Nf;  
+
+# # or
+# N = x.size
+# Nf = nextpow2(N)
+# fp = np.fft.fftshift(np.fft.fftfreq(Nf, d=T0))
+ 
+# -Représentation fréquentiel
+plt.figure()
+# plt.subplot(312)
+plt.stem(fp,Xa)
+plt.xlabel('fréquence (Hz)')
+plt.ylabel('spectre d''amplitude')
