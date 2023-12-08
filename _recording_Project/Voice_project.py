a6e437b71f3ca29a4492ed78fa37745d6010d249
yasser-jemli <yasserjamli37@gmail.com>
2023-12-08 12:38:56 +0100

    fixing Comments
    Signed-off-by: yasser-jemli <yasserjamli37@gmail.com>

--- _recording_Project/Voice_project.py
+++ _recording_Project/Voice_project.py
@@ -10,6 +10,7 @@ from python_speech_features import logfbank
 import scipy.io.wavfile as wav
 
 # ''' recording wave speech signal '''
+# => Code : 
 # fs = 16000  # Sample rate
 # seconds = 3  # Duration of recording
 # print('start recording')
@@ -17,17 +18,22 @@ import scipy.io.wavfile as wav
 # sd.wait()  # Wait until recording is finished
 # write('output.wav', fs, x)  # Save as WAV file
 
+# play the saved wav 
 # sd.play(20*x, fs)
 # status = sd.wait()  # Wait until file is done playing
 # print('fin recording')
 
+# PLay the saved signal 
+# => Code : 
 # (fs,x) = wav.read("/home/yasser_jemli/Signal_Processing_Project/_recording_Project/output.wav")
 # sd.play(20*x, fs) 
 # sd.wait()
 
+# Read the SA1.WAV signal 
 (fs,x) = wav.read("/home/yasser_jemli/Signal_Processing_Project/_recording_Project/SA1.WAV")
 sd.play(20*x, fs)
 
+# PLot the SA1.WAV signal
 time=np.arange(0,len(x))/fs; # Time vector on x-axis
 plt.figure()
 plt.subplot(311)
