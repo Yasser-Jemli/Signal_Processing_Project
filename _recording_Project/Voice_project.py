958dc92fd4bdf88513643acd40523c80ba7a51d5
yasser-jemli <yasserjamli37@gmail.com>
2023-12-08 11:40:30 +0100

    Fixing Path to read wav file
    Signed-off-by: yasser-jemli <yasserjamli37@gmail.com>

--- _recording_Project/Voice_project.py
+++ _recording_Project/Voice_project.py
@@ -9,16 +9,18 @@ from python_speech_features import mfcc, delta
 from python_speech_features import logfbank
 import scipy.io.wavfile as wav
 
-''' recording wave speech signal '''
-fs = 16000  # Sample rate
-seconds = 3  # Duration of recording
-print('start recording')
-x = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-sd.wait()  # Wait until recording is finished
-write('output.wav', fs, x)  # Save as WAV file
+# ''' recording wave speech signal '''
+# fs = 16000  # Sample rate
+# seconds = 3  # Duration of recording
+# print('start recording')
+# x = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+# sd.wait()  # Wait until recording is finished
+# write('output.wav', fs, x)  # Save as WAV file
 
-sd.play(20*x, fs)
-status = sd.wait()  # Wait until file is done playing
-print('fin recording')
+# sd.play(20*x, fs)
+# status = sd.wait()  # Wait until file is done playing
+# print('fin recording')
 
+(fs,x) = wav.read("/home/yasser_jemli/Signal_Processing_Project/_recording_Project/output.wav")
+sd.play(x, fs) 
 
